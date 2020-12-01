7289ca1d8121263c0e0c7088cc07944159f225c1
sofia.brown <sofia.brown@unity3d.com>
2020-12-01 11:45:31 -0500

    Sample Package for git

---
+++ Editor/ImmutableScene.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class ImmutableScene : AssetModificationProcessor
+{
+    static string[] OnWillSaveAssets(string[] paths)
+    {
+        string scenePath = "Packages/com.unity.enginesupport.esgittest/Runtime/Sample.unity";
+        List<string> pathsToSave = new List<string>();
+        foreach (string path in paths)
+        {
+            Debug.Log(path);
+            //if (!path.Equals(scenePath))
+                pathsToSave.Add(path);
+        }
+        return pathsToSave.ToArray();
+    }
+}
